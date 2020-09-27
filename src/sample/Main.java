7e5ef7ae128f45b7cd645b3feaa2a76962b3565a
Valery1508 <lera_zubritskaya00@mail.ru>
2020-09-28 01:11:26 +0300

    commit2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,26 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.view.MainFrame;
+
+import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        String userName = "root";
+        String password = "root";
+        String connectionUrl = "jdbc:mysql://localhost:3306/marketing";
+        Class.forName("com.mysql.jdbc.Driver");
+        try(Connection connection = DriverManager.getConnection(connectionUrl, userName, password)){
+            System.out.println("we're connected");
+        }
+
+        MainFrame mainFrame = new MainFrame(primaryStage);
+        mainFrame.show();
     }
 
 
